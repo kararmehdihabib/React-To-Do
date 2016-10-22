09c86be0ef7bbbdcd6875db96a6c5ee05169a18c
kararmehdi <karar.habib2@metropolia.fi>
2016-10-23 02:32:16 +0300

    Add Toggle Support

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -12,5 +12,21 @@ it('should exist', ()=>{
     expect(Todo).toExist();
 });
     
+it('should call onToggle prop with id on click', ()=>{
+    var todoData = {
+        id:199,
+        text: 'Write',
+        completed:true
+        
+    };
+    var spy=expect.createSpy();
+    var todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy}></Todo>);
+    var $el = $(ReactDOM.findDOMNode(todo));
+    
+    TestUtils.Simulate.click($el[0]);
+    
+    expect(spy).toHaveBeenCalledWith(199);
+    
+});    
          
 });
