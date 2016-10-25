ed4d5cedefc2dd7724be81ba0f697acf74ac6587
kararmehdi <karar.habib2@metropolia.fi>
2016-10-25 13:04:07 +0300

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -19,5 +19,29 @@ module.exports = {
         
         return $.isArray(todos) ? todos : [];
        
+    },
+    filterTodos: function(todos, showCompleted, searchText){
+        var filteredTodos = todos;
+         //filter by show completed
+            filteredTodos = filteredTodos.filter((todo)=>{
+                return todo.completed ===false || todo.completed === showCompleted;
+            });
+        //filter by search text
+        filteredTodos = filteredTodos.filter((todo)=>{
+            var text = todo.text.toLowerCase();
+            return searchText.length ===0 || text.indexOf(searchText)>-1;
+        });
+        //sort todos with non-completed first
+        filteredTodos.sort((a,b)=>{
+            if(a.completed === true && b.completed ===false){
+                
+                return 1;
+            } else if(a.completed===false &&  b.completed===true){
+                return -1;
+            }else{
+                return 0;
+            }
+        });
+        return filteredTodos;
     }
 };
